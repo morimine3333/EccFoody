41dfbce8ac4b224d52e2bf98282d08d449a6b4a6
2180282 <2180282ecccomp@gmail.com>
2020-01-23 12:26:08 +0900

    お店登録画面　 レイアウト追加

---
+++ app/src/main/java/com/example/a2170188/navigationdrawractivity/NewStore.java
@@ -0,0 +1,14 @@
+package com.example.a2170188.navigationdrawractivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewStore extends AppCompatActivity {
+
+
+}
